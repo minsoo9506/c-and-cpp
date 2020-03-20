40c625163d6df8c347f9c0b9871a8d3d34144570
minsoo9506 <ghktjd15gh@gmail.com>
2020-03-20 21:54:11 +0900

    0320algorithm

---
+++ c++ algorithm/011.숫자총갯수.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, tmp;
+	int cnt = 0;
+	cin >> N;
+
+	for (int i = 1; i <= N; ++i)
+	{
+		tmp = i;
+		while (tmp > 0)
+		{
+			tmp /= 10;
+			cnt++;
+		}
+	}
+	cout << cnt << endl;
+
+	return 0;
+}
