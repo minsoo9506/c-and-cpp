b7b994d91582f84619f238e1ddbd790f051b0f06
minsoo9506 <ghktjd15gh@gmail.com>
2020-03-21 12:44:16 +0900

    0321algorithm

---
+++ c++ algorithm/012.숫자총갯수(large).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long N, sum(0), answer(0), nine(9);
+	int cnt(0);
+
+	cin >> N;
+
+	while (nine < N)
+	{
+		cnt++;
+		sum += nine;
+		answer += sum * cnt;
+		nine *= 10;
+	}
+	cnt++;
+	answer += (N - sum) * cnt;
+	
+	cout << answer << endl;
+
+	return 0;
+}
