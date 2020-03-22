083ac8963dfe4dcc4239cd0487ad9d1dc9cd5ecf
minsoo9506 <ghktjd15gh@gmail.com>
2020-03-22 17:42:53 +0900

    0322algorithm

---
+++ c++ algorithm/013.가장많이나온수.cpp
@@ -0,0 +1,34 @@
+﻿// 가장 많이 나온 수
+// 100의 자리수에서 가장 많이 사용된 수를 return
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char N[101]; // 자연수 100자리 넣도록 (null character +1)
+	int digit = 0;
+	int count[10] = {};
+	cin >> N;
+
+	for (int i = 0; N[i] != '\0' ; ++i)
+	{
+		digit = N[i] - 48;
+		count[digit]++;
+	}
+
+	int max = 0;
+	int answer = 0;
+
+	for (int i = 0; i < 10; ++i)
+	{
+		if (count[i] >= max)
+		{
+			max = count[i];
+			answer = i;
+		}
+	}
+
+	cout << answer << endl;
+
+	return 0;
+}
