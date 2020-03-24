06ca2e3cfa84467bfad444680929eb4c8c74e611
minsoo9506 <ghktjd15gh@gmail.com>
2020-03-24 18:13:04 +0900

    0324algorithm

---
+++ c++ algorithm/015.소수의갯수.cpp
@@ -0,0 +1,27 @@
+﻿/// 소수의 갯수
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int x) {
+	bool flag = true;
+	for (int i = 2; i*i <= x; ++i) {
+		if (x % i == 0) {
+			flag = false;
+			break;
+		}
+	}
+	return flag;
+}
+
+int main() 
+{
+	int n, cnt(0);
+	cin >> n;
+
+	for (int i = 2; i <= n; i++) 
+		if (isPrime(i)) cnt++;
+	cout << cnt << endl;
+
+	return 0;
+}
