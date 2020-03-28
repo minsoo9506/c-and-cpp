8272e5731dbd16cad23b96fa7935a3027f3e39a6
minsoo9506 <ghktjd15gh@gmail.com>
2020-03-28 10:23:04 +0900

    0328algorithm

---
+++ c++ algorithm/019.앞사람이 큰 경우.cpp
@@ -0,0 +1,27 @@
+﻿// 바로 앞의 수가 더 큰 경우 count
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	// input data 만들기
+	int N;
+	cin >> N;
+	int* p = new int[N];
+	for (int i = 0; i < N; i++)
+	{
+		cin >> p[i];
+	}
+	// 실행
+	int cnt(0);
+	for (int i = 0; i < N - 1; ++i)
+	{
+		if (p[i] > p[i + 1]) cnt++;
+	}
+
+	cout << cnt << endl;
+	delete[] p;
+
+	return 0;
+}
