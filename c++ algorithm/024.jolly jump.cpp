bd006589e4b8bb006c68659a2500f5d36590984f
minsoo9506 <ghktjd15gh@gmail.com>
2020-04-02 21:08:12 +0900

    0402algorithm

---
+++ c++ algorithm/024.jolly jump.cpp
@@ -0,0 +1,35 @@
+﻿// jolly jumper
+
+#include <iostream>
+#include <vector>
+#include <algorithm> // abs
+#include <numeric>   // accumulate
+using namespace std;
+
+int main() 
+{
+	// input data
+	int N;
+	cin >> N;
+	vector<int> num(N);
+	for (int i = 0; i < N; ++i)
+		cin >> num[i];
+
+	// algorithm
+	int index = 0;
+	vector<int> data(N); // 0으로 초기화
+	for (int i = 0; i < N - 1; ++i)
+	{
+		index = abs(num[i] - num[i + 1]);
+		if (index >= 0 && index < N && data[index] == 0)
+			data[index] = 1;
+		else
+		{
+			cout << "NO" << endl;
+			return 0;
+		}
+	}
+	cout << "YES" << endl;
+
+	return 0;
+}
