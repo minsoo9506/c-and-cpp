89acf3c7c4045807985b2410acef0256a59cedb8
minsoo9506 <ghktjd15gh@gmail.com>
2020-04-04 11:44:47 +0900

    0404algorithm

---
+++ c++ algorithm/026.마라톤.cpp
@@ -0,0 +1,30 @@
+﻿// 마라톤
+// 앞에 본인보다 작은 수가 있으면 추월이 가능하다
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+	// input data
+	int N;
+	cin >> N;
+	vector<int> ability(N);
+	for (int i = 0; i < N; ++i)
+		cin >> ability[i];
+
+	// algorithm
+	vector<int> final(N);
+	for (int i = 0; i < N; ++i)
+	{
+		final[i] = i+1;
+		for (int j = 0; j < i; ++j)
+			if (ability[i] > ability[j]) final[i]--;
+	}
+
+	for (int i = 0; i < N; ++i)
+		cout << final[i] << ' ';
+
+	return 0;
+}
