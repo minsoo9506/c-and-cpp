3f2070098c8c23a73034e9a1a10d8c797edcde92
minsoo9506 <ghktjd15gh@gmail.com>
2020-04-06 16:46:28 +0900

    0406algorithm

---
+++ c++ algorithm/028.N!에서 0의 갯수.cpp
@@ -0,0 +1,38 @@
+﻿// N! 소인수분해
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+	// input data
+	int N;
+	cin >> N;
+
+	// algorithm
+	vector<int> cnt = { 0,0 };
+	int tmp(0);
+
+	for (int i = 1;i <= N; ++i)
+	{
+		tmp = i;   
+		while (tmp % 2 == 0) 
+		{
+			tmp /= 2;
+			cnt[0]++;
+			if (tmp == 1) break;
+		}
+		while (tmp % 5 == 0)
+		{
+			tmp /= 5;
+			cnt[1]++;
+			if (tmp == 1) break;
+		}
+	}
+
+	cout << min(cnt[0], cnt[1]) << endl;
+
+	return 0;
+}
