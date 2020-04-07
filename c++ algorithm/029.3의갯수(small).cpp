a21b524020e1083a597cbd499b44bcb43d52149a
minsoo9506 <ghktjd15gh@gmail.com>
2020-04-08 00:56:07 +0900

    0408algorithm

---
+++ c++ algorithm/029.3의갯수(small).cpp
@@ -0,0 +1,28 @@
+﻿// 3의 갯수는? small
+#include <iostream>
+// #include <vector>
+// #include <algorithm>
+using namespace std;
+
+int main() 
+{
+	// input data
+	int num = 0;
+	cin >> num;
+
+	// algorithm
+	int tmp(0), cnt(0);
+	for (int i = 0; i <= num; i++) // 숫자가 크지 않기에 하나씩 확인
+	{
+		tmp = i;
+		while (tmp > 0)
+		{
+			if (tmp % 10 == 3) cnt++; // 나머지가 3인 경우 cnt++
+			tmp /= 10;
+		}		
+	}
+
+	cout << cnt << endl;
+
+	return 0;
+}
