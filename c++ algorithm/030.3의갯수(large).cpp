5e60fffc54de3c322ff05bed1565f126083e3b8f
minsoo9506 <ghktjd15gh@gmail.com>
2020-04-08 11:50:11 +0900

    0408algorithm

---
+++ c++ algorithm/030.3의갯수(large).cpp
@@ -0,0 +1,31 @@
+﻿// 3의 갯수는? large
+#include <iostream>
+// #include <vector>
+// #include <algorithm>
+using namespace std;
+
+int main() 
+{
+	// input data
+	long num = 0;
+	cin >> num;
+
+	// algorithm
+	int cnt(0), left(0), current(0), right(0), k(1);
+	while (left > 0)
+	{
+		left = num / (k * 10);
+		current = num % (k * 10);
+		right = num % k;
+
+		if (current > 3) cnt += (left + 1) * k;
+		else if (current == 3) cnt += (left * k) + (right + 1);
+		else cnt += left * k;
+
+		k *= 10;
+	}
+
+	cout << cnt << endl;
+
+	return 0;
+}
