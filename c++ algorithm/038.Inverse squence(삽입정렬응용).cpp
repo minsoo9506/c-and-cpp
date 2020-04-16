b9c9c0cb1364c54f2fda36a7bd5405f09b027e6b
minsoo9506 <ghktjd15gh@gmail.com>
2020-04-16 22:39:36 +0900

    0416algorithm

---
+++ c++ algorithm/038.Inverse squence(삽입정렬응용).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+	// input data
+	int n;
+	cin >> n;
+	vector<int> input(n);
+	vector<int> output(n);
+	for (int i = 0; i < n; ++i)
+		cin >> input[i];
+
+	// algorithm
+	int pos(0);
+	for (int i = n-1; i >= 0; --i)
+	{
+		pos = i;
+		for (int j = 0; j < input[i] ; ++j)
+		{
+			output[pos] = output[pos + 1];
+			pos++;
+		}
+		output[pos] = i;
+	}
+
+	for (int i = 0; i < n; ++i)
+		cout << output[i] << ' ' ;
+
+	return 0;
+}
