58932cae2538332bd1e829a4ef567ba6569b6916
minsoo9506 <ghktjd15gh@gmail.com>
2020-04-19 15:08:11 +0900

    0419 algorithm

---
+++ c++ algorithm/041.연속된 자연수의 합.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+	// input data
+	int n;
+	cin >> n;
+
+	// algorithm
+	int tmp(0), b(2), cnt(0);
+	tmp = n - 1;
+
+	while (tmp > 0)
+	{
+		tmp -= b;
+		if (tmp % b == 0)
+		{
+			for (int i = 1; i < b; ++i)
+				cout << (tmp / b) + i << " + ";
+			cout << (tmp / b) + b << " = " << n << endl;
+			cnt++;
+		}
+		b++;
+	}
+
+	cout << cnt << endl;
+
+	return 0;
+}
