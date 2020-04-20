7bd5d9c04518b528a0d72fd8f526556e104490f7
minsoo9506 <ghktjd15gh@gmail.com>
2020-04-20 18:09:47 +0900

    0420algorithm

---
+++ c++ algorithm/042.이분탐색.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+	// input data
+	int n, target;
+	cin >> n >> target;
+	vector<int> arr(n);
+	for (int i = 0; i < n; ++i)
+		cin >> arr[i];
+
+	// algorithm
+	sort(arr.begin(), arr.end()); // 정렬
+
+	int left(0), mid(0), right(n-1);
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+
+		if (target == arr[mid])
+		{
+			cout << mid + 1 << "번째" << endl;
+			break;
+		}
+		else if (target > arr[mid]) right = mid - 1;
+		else left = mid+1;
+	}
+
+	return 0;
+}
