50afc773c809019ce8b6c81804f4177139c754b4
minsoo9506 <ghktjd15gh@gmail.com>
2020-04-25 10:04:11 +0900

    0425 algorithm upload

---
+++ c++ algorithm/047. 봉우리찾기(2차원배열).cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+	// input
+	int n(0);
+	cin >> n;
+	int arr[51][51] = { 0 };
+	for (int i = 1; i < n + 1; ++i)
+	{
+		for (int j = 1; j < n + 1; ++j)
+		{
+			cin >> arr[i][j];
+		}
+	}
+
+	// algorithm
+	int cnt(0), flag(0);
+	int x[4] = {-1, 0, +1, 0};
+	int y[4] = { 0, -1, 0, +1 };
+	for (int i = 1; i < n + 1; ++i)
+	{
+		for (int j = 1; j < n + 1; ++j)
+		{
+			flag = 0;
+			for (int k = 0; k < 4; ++k)
+				if (arr[i][j] <= arr[i + x[k]][j + y[k]]) // 주위에 하나라도 본인보다 큰게 있으면 탈락
+				{
+					flag = 1;
+					break;
+				}
+			if (flag == 0)
+				cnt++;
+		}
+	}
+
+	cout << cnt << endl;
+
+	return 0;
+}
