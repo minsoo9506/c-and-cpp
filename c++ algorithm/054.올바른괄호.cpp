7af42fc0fc08cf8defc5a6ccc310ac61535dbd84
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-03 14:22:01 +0900

    0503 algorithm upload

---
+++ c++ algorithm/054.올바른괄호.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main()
+{
+	stack<char> s;
+	char input[30];
+	// input
+	cin >> input;
+	
+	// algorithm
+	int flag(1); // YES 의미
+
+	for (int i = 0; input[i] != '\0'; ++i)
+	{
+		if (input[i] == '(') s.push('(');
+		else
+		{
+			if (s.empty())
+			{
+				flag = 0;
+				break;
+			}
+			s.pop();
+		}
+	}
+
+	if (flag) cout << "YES" << endl;
+	else cout << "NO" << endl;
+
+	return 0;
+}
