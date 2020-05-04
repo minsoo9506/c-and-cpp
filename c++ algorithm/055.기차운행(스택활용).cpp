0e814cc3f8014ce5fb5ae8151a783af1ddc05c95
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-04 17:54:59 +0900

    0504 algorithm upload

---
+++ c++ algorithm/055.기차운행(스택활용).cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	// input
+	int n;
+	cin >> n;
+	vector<int> input(n);
+	for (int i = 0; i < n; ++i)
+		cin >> input[i];
+	
+	// algorithm
+	int num(1);
+	stack<int> s;
+	vector<char> output;
+
+	for (int i = 0; i < n; ++i)
+	{
+		s.push(input[i]);
+		output.push_back('P');
+		while (true)
+		{
+			if (s.empty()) break;
+			else
+			{
+				if (s.top() == num)
+				{
+					s.pop();
+					num++;
+					output.push_back('O');
+				}
+				else break;
+			}
+		}
+	}
+
+	if (!s.empty()) cout << "impossible" << endl;
+	else
+	{
+		for (int i = 0; i < n; ++i)
+			cout << output[i];
+	}
+
+	return 0;
+}
