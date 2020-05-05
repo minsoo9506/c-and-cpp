001bdb46ddc9818357959c0fe6c0a7122b375da6
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-05 12:41:21 +0900

    0505 algorithm upload

---
+++ c++ algorithm/056재귀함수기본.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int rec(int n)
+{
+	if (n == 0) return 0;
+	else
+	{
+		rec(n - 1);
+		cout << n << ' ';
+	}
+}
+
+int main()
+{
+	// input
+	int n;
+	cin >> n;
+	
+	// algorithm
+	rec(n);
+
+	return 0;
+}
