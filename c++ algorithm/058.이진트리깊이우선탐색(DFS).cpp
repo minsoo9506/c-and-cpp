e3c5a6cac7f55a1466ac4be07b481f33b5bcff12
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-07 17:52:59 +0900

    0507 algorithm upload

---
+++ c++ algorithm/058.이진트리깊이우선탐색(DFS).cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void rec(int v)
+{
+	if (v > 7) return;
+	else
+	{
+		rec(v * 2);
+		rec(v * 2 + 1);
+		cout << v << ' '; // 후향
+	}
+}
+
+int main()
+{
+	// input
+	int n;
+	cin >> n;
+	
+	// algorithm
+	rec(n);
+
+	return 0;
+}
