80842d25a010977b11808531ac9db7c6d88fed7f
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-09 16:30:22 +0900

    5009 algorithm

---
+++ c++ algorithm/059.부분집합(DFS).cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, ch[10];
+
+void DFS(int L)
+{
+	if (L == n + 1)
+	{
+		for (int i = 0; i < n; ++i)
+			if (ch[i] == 1) cout << i + 1 << ' ';
+	}
+	else
+	{
+		ch[L] = 1;
+		DFS(L + 1);
+		ch[L] = 0;
+		DFS(L + 1);
+	}
+}
+
+int main()
+{
+	// input
+	int n;
+	cin >> n;
+	
+	// algorithm
+	DFS(1);
+
+	return 0;
+}
---
+++ c++ algorithm/060.합이같은부분집합(DFS).cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, input[10];
+int total(0);
+int flag(0);
+
+void DFS(int depth, int sum)
+{	
+	if (flag) return;
+	if (total == sum * 2) flag = 1;
+	if (depth == n) return;
+	else
+	{
+		DFS(depth + 1, sum + input[depth]);
+		DFS(depth + 1, sum);
+	}
+}
+
+int main()
+{
+	// input
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> input[i];
+		total += input[i];
+	}
+		
+	// algorithm
+	DFS(0, 0);
+
+	if (flag) cout << "YES" << endl;
+	else cout << "NO" << endl;
+
+	return 0;
+}
