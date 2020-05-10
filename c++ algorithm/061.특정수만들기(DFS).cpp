3e5f20faf7f0747ceb24387823f865e168e99ada
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-10 14:57:15 +0900

    0510 algorithm upload

---
+++ c++ algorithm/061.특정수만들기(DFS).cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, input[10];
+int total(0);
+int cnt(0);
+
+void DFS(int depth, int sum)
+{	
+	if (depth == n)
+	{
+		if (total == sum) cnt++;
+		return;
+	}
+	else
+	{
+		DFS(depth + 1, sum + input[depth]);
+		DFS(depth + 1, sum);
+		DFS(depth + 1, sum - input[depth]);
+	}
+}
+
+int main()
+{
+	// input
+	cin >> n >> total;
+	for (int i = 0; i < n; ++i)
+		cin >> input[i];
+		
+	// algorithm
+	DFS(0, 0);
+
+	if (cnt) cout << cnt << endl;
+	else cout << -1 << endl;
+
+	return 0;
+}
