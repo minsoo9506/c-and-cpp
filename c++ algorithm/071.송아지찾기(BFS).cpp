4b2da483fb6f52bc82a9152673d1c6aa6406dc48
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-20 21:41:36 +0900

    0520 algorithm upload

---
+++ c++ algorithm/071.송아지찾기(BFS).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{	
+	// input
+	int s(0), e(0);
+	cin >> s >> e;
+
+	// algorithm
+	int x(0), pos(s);
+	int d[3] = { -1,1,5 };
+	int ch[100];
+	queue<int> Q;
+
+	ch[s] = 0;
+	Q.push(s);
+	while (!Q.empty())
+	{
+		x = Q.front();
+		Q.pop();
+		for (int i = 0; i < 3; ++i)
+		{
+			pos += d[i];
+			if (pos == e)
+			{
+				cout << ch[x] + 1 << endl;
+				exit(0);
+			}
+
+			if (ch[pos] == 0)
+			{
+				ch[pos] = ch[x] + 1;
+				Q.push(pos);
+			}
+		}
+	}
+	return 0;
+}
