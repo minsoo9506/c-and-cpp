5d11839f02b217ec17777ecd6bc3fccb39a05c30
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-21 19:00:00 +0900

    0521 algorithm upload

---
+++ c++ algorithm/072.공주구하기(큐).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{	
+	// input
+	int n(0), k(0);
+	cin >> n >> k;
+
+	// algorithm
+	queue<int> Q;
+
+	while (!Q.empty())
+	{
+		for (int i = 1; i < k; ++i)
+		{
+			Q.push(Q.front);
+			Q.pop();
+		}
+		Q.pop();
+		if (Q.size() == 1)
+		{
+			cout << Q.front << endl;
+			Q.pop();
+		}
+	}
+	return 0;
+}
