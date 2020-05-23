f6cbfe9ab33d9cb8054bf7ecf5681bbfb4a89c8d
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-23 22:48:50 +0900

    0523 algorithm upload

---
+++ c++ algorithm/074.최소(priority_queue).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main()
+{	
+	// input
+	int n(0);
+
+	// algorithm
+	priority_queue <int> pQ;
+
+	while (true)
+	{
+		cin >> n;
+		if (n == -1) break;
+		else if (n == 0)
+		{
+			if (pQ.empty()) cout << -1 << endl;
+			else
+			{
+				cout << -pQ.top() << endl;
+				pQ.pop();
+			}
+		}
+		else pQ.push(-n);
+	}
+
+	return 0;
+}
