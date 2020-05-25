639b51f6ba6c4d39dbe61cac68fcf2a0409636c9
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-25 22:17:13 +0900

    0525 algorithm upload

---
+++ c++ algorithm/076.이항계수(DFS 메모이제이션).cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int arr[20][20];
+
+int DFS(int n, int m)
+{
+	if (arr[n][m] != 0) return arr[n][m]; // DFS에서 중복된 부분을 계산하지 않고 메모이제이션!
+	if (n == m || m == 0) return 1;
+	else return arr[n][m] = DFS(n - 1, m - 1) + DFS(n - 1, m);
+}
+
+int main()
+{
+	// input
+	int n(0), m(0);
+	cin >> n >> m;
+
+	// algorithm
+	cout << DFS(n, m) << endl;
+
+	return 0;
+}
