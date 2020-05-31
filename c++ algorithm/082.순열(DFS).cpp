d342843afa9a615f4b4174b471358db7668ae56a
minsoo9506 <ghktjd15gh@gmail.com>
2020-05-31 18:09:19 +0900

    0531 algorithm upload

---
+++ c++ algorithm/082.순열(DFS).cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, r, cnt(0);
+int a[10], res[10], ch[10];
+
+int DFS(int L)
+{
+	if (L == r)
+	{
+		for (int j = 0; j < L; ++j)
+			cout << res[j] << endl;
+		cnt++;
+	}
+	else
+		for(int i = 1 ; i <=n ; ++i)
+			if (ch[i] == 0)
+			{
+				res[L] = a[i];
+				ch[i] = 1;
+				DFS(L + 1);
+				ch[i] = 0;
+			}
+}
+
+int main()
+{
+	// input
+	cin >> n >> r;
+	for (int i = 0; i < n; ++i)
+		cin >> a[i];
+
+	// algorithm
+	DFS(0);
+	cout << cnt << endl;
+
+	return 0; 
+}
